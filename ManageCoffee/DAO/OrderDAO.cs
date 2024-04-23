eb3b60fc3507d5f55f3f3437f98d5e7114383db0
CaoHuy <caohuy.cantho@gmail.com>
2024-04-23 15:36:32 +0700

    Huy đã fix xong Dashboard

--- ManageCoffee/DAO/OrderDAO.cs
+++ ManageCoffee/DAO/OrderDAO.cs
@@ -186,7 +186,7 @@ namespace ManageCoffee.DAO
             {
                 if (ints.Contains(item.OrderId))
                 {
-                    // numberProduct += item.Quantity;
+                    numberProduct += item.Quantity ?? 0;
                 }
             }
             return numberProduct;
